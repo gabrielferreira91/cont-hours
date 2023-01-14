3fc25daf2ec90281776b6f29dce95f01f09a02c6
Gabriel Ferreira <ferreira.gabriel@live.com>
2023-01-14 14:25:25 -0300

    changes: add value of working hours

--- contTime.js
+++ contTime.js
@@ -1,139 +1,159 @@
-let inputEnterHour = document.querySelector('#hourEnter');
-let inputEnterMinute = document.querySelector('#minuteEnter');
+let inputEnterHour = document.querySelector('#hourEnter')
+let inputEnterMinute = document.querySelector('#minuteEnter')
 
-let inputEndHour = document.querySelector('#hourEnd');
-let inputEndMinute = document.querySelector('#minuteEnd');
+let inputEndHour = document.querySelector('#hourEnd')
+let inputEndMinute = document.querySelector('#minuteEnd')
+
+let inputValue = document.querySelector('.hourValue')
 
 let totalHour = document.querySelector('.totalHour')
 let totalMinute = document.querySelector('.totalMinute')
 
 let calcTimeButton = document.querySelector('.calcTime')
 
-let arrayForNumbers = [];
-let formatted = [];
+let totalValue = document.querySelector('.totalValue')
 
+let arrayForNumbers = []
+let formatted = []
 
-//TROCAR DE INPUT AUTOMATICAMENTE 
-var container1 = document.getElementsByClassName("timeWorked")[0];
-container1.onkeyup = function(e) {
-    var target = e.srcElement;
-    var maxLength = 2;
-    var myLength = target.value.length;
-    if (myLength >= maxLength) {
-        var next = target;
-        while (next = next.nextElementSibling) {
-            if (next == null)
-                break;
-            if (next.tagName.toLowerCase() == "input") {
-                next.focus();
-                break;
-            }
-        }
+//TROCAR DE INPUT AUTOMATICAMENTE
+let container1 = document.getElementsByClassName('timeWorked')[0]
+container1.onkeyup = function (e) {
+  let target = e.srcElement
+  let maxLength = 2
+  let myLength = target.value.length
+  if (myLength >= maxLength) {
+    let next = target
+    while ((next = next.nextElementSibling)) {
+      if (next == null) break
+      if (next.tagName.toLowerCase() == 'input') {
+        next.focus()
+        break
+      }
     }
+  }
 }
 
-inputEnterMinute.onkeyup = function(e) {
-    let target = e.srcElement;
-    let myLength = target.value.length 
-    var max = 2;
+inputEnterMinute.onkeyup = function (e) {
+  let target = e.srcElement
+  let myLength = target.value.length
+  let max = 2
 
-    if (myLength >= max){
-        inputEndHour.focus()
-    } 
+  if (myLength >= max) {
+    inputEndHour.focus()
+  }
 }
 
-inputEndMinute.onkeyup = function(e) {
-    let target = e.srcElement;
-    let myLength = target.value.length 
-    var max = 2;
+inputEndMinute.onkeyup = function (e) {
+  let target = e.srcElement
+  let myLength = target.value.length
+  let max = 2
 
-    if (myLength >= max){
-        calcTimeButton.focus()        
-    } 
+  if (myLength >= max) {
+    inputValue.focus()
+  }
 }
 
 //TROCAR DE INPUT AUTOMATICAMENTE TERMINA
 
+function getTimes() {
+  arrayForNumbers.length = 0
+  formatted.length = 0
 
+  let enterHour = parseInt(inputEnterHour.value)
+  let enterMinute = parseInt(inputEnterMinute.value)
+  let endHour = parseInt(inputEndHour.value)
+  let endMinute = parseInt(inputEndMinute.value)
+  let hourValue = parseFloat(inputValue.value)
 
-function getTimes(){
-    arrayForNumbers.length = 0;  
-    formatted.length = 0;  
-    
-        
-    let enterHour = parseInt(inputEnterHour.value);
-    let enterMinute = parseInt(inputEnterMinute.value);
-    let endHour = parseInt(inputEndHour.value);
-    let endMinute = parseInt(inputEndMinute.value);   
-    
+  let totalMinutes = calcHoursWorked(enterHour, enterMinute, endHour, endMinute)
+  let resultHour = convertMinuteInHourReturnHour(totalMinutes)
+  let resultMinute = convertMinuteInHourReturnMinute(totalMinutes)
 
-    let totalMinutes = calcHoursWorked(enterHour,enterMinute,endHour,endMinute);
-    let resultHour = convertMinuteInHourReturnHour(totalMinutes);
-    let resultMinute = convertMinuteInHourReturnMinute(totalMinutes);
-   
-    arrayForNumbers.push(resultHour);
-    arrayForNumbers.push(resultMinute);
+  let valueTotal = calcValueHour(hourValue, totalMinutes)
 
-    arrayForNumbers.forEach(num => {
-        formatted.push(
-          num.toLocaleString('pt-BR', {//this is the function that formats the numbers
-            minimumIntegerDigits: 2, //change this to your minimum length
-            useGrouping: false
-          })
-        )
-    })
-   
-    if(validatedTime(enterHour, enterMinute) && validatedTime(endHour, endMinute))
-    {
-        if (totalMinutes >= 0) {
-            setTimes(formatted[0], formatted[1]);
-        } else {
-            msgError()
-        } 
-    }else {
-        return msgError();
+  setValue(valueTotal)
+
+  console.log(valueTotal)
+
+  arrayForNumbers.push(resultHour)
+  arrayForNumbers.push(resultMinute)
+
+  arrayForNumbers.forEach(num => {
+    formatted.push(
+      num.toLocaleString('pt-BR', {
+        //this is the function that formats the numbers
+        minimumIntegerDigits: 2, //change this to your minimum length
+        useGrouping: false
+      })
+    )
+  })
+
+  if (
+    validatedTime(enterHour, enterMinute) &&
+    validatedTime(endHour, endMinute)
+  ) {
+    if (totalMinutes >= 0) {
+      setTimes(formatted[0], formatted[1])
+    } else {
+      msgError()
     }
- }
-
-
-function setTimes(resultHour, resultMinute){   
-    totalHour.textContent = resultHour;
-    totalMinute.textContent = resultMinute;
+  } else {
+    return msgError()
+  }
 }
 
-function convertHourInMinute(hour){
-    return hour*60
+function setTimes(resultHour, resultMinute) {
+  totalHour.textContent = resultHour
+  totalMinute.textContent = resultMinute
 }
 
-function convertMinuteInHourReturnHour(minute){
-    return Math.floor(minute/60)
-}
-function convertMinuteInHourReturnMinute(minute){
-    return minute % 60
+function convertHourInMinute(hour) {
+  return hour * 60
 }
 
-function calcHoursWorked(enterHour, enterMinute, endHour, endMinute){
-    let hourEnter = convertHourInMinute(enterHour);
-    let resultEnter = hourEnter + enterMinute;
-
-    let hourEnd = convertHourInMinute(endHour);
-    let resultEnd = hourEnd + endMinute;
-
-    return resultEnd - resultEnter
+function convertMinuteInHourReturnHour(minute) {
+  return Math.floor(minute / 60)
+}
+function convertMinuteInHourReturnMinute(minute) {
+  return minute % 60
 }
 
-
-function validatedTime(hour, minute){
-    if (hour >= 0 && hour <=24 ) { 
-        if (minute >= 0 && minute < 60) {
-            return true
-        }
-        return false
-    }  
-    return false    
+function calcValueHour(value, time) {
+  if (value >= 0) {
+    return (value * time) / 60
+  } else {
+    return 0
+  }
 }
 
-function msgError(){
-    totalHour.textContent = 'HORAS';
-    totalMinute.textContent = 'INVÁLIDAS';
+function setValue(value) {
+  value = parseFloat(value.toFixed(2))
+  totalValue.textContent = value.toFixed(2)
+}
+
+function calcHoursWorked(enterHour, enterMinute, endHour, endMinute) {
+  let hourEnter = convertHourInMinute(enterHour)
+  let resultEnter = hourEnter + enterMinute
+
+  let hourEnd = convertHourInMinute(endHour)
+  let resultEnd = hourEnd + endMinute
+
+  return resultEnd - resultEnter
+}
+
+function validatedTime(hour, minute) {
+  if (hour >= 0 && hour <= 24) {
+    if (minute >= 0 && minute < 60) {
+      return true
+    }
+    return false
+  }
+  return false
+}
+
+function msgError() {
+  totalHour.textContent = ''
+  totalMinute.textContent = 'INVÁLIDO'
+  totalValue.textContent = '0.00'
 }
